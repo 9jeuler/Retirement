18a2f3b2b26d406583172b28e5d237790ad32659
9jeuler <31480902+9jeuler@users.noreply.github.com>
2017-08-31 09:19:55 -0500

    Started working

---
+++ retirement.js
@@ -0,0 +1,38 @@
+//Function declarations
+function retire(age){
+    let year(16); //set current year
+    let retirementAge(65); //set current retirement age
+    let diff = retirementAge - age;
+
+    //write three separate conditional statements to say when you can take out money.
+
+    //first if your age is older than the retirementAge
+    if (diff > 0){
+    console.log("Sorry kid, not old enough");
+}
+    //second if your age is equal to the retirementAge
+    if (diff = 0){
+        console.log("Welcome to retirement!");
+    }
+    //third if your age is younger than the retirementAge
+    if (diff < 0){
+        console.log("The Money is in the mail");
+    }
+}
+
+
+//main function tests your code above with different inputs.
+function main(){
+    retire(18);
+    retire(50);
+    ret6re(65);
+    retire(100); //Hundo!
+
+}
+
+
+
+
+// ctrl + shift + B
+//This will run the program.
+main();
